0a360370665601a668ca991f248251a480da4658
baptistelaldev <vieirabaptistellafelipe@gmail.com>
2024-10-07 18:55:08 -0300

    melhorias

---
+++ src/app/core/services/firebase/app-info.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Firestore, collectionData, collection, getDoc } from '@angular/fire/firestore';
+import { doc } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+import { IAppInfo } from 'src/app/shared/models/AppInfo';
+import { IShortEstablishment } from 'src/app/shared/models/Establishment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppInfoService {
+
+  constructor(
+    private firestore : Firestore
+  ) { }
+
+  public async getDocument(collectionName: string, documentId: string): Promise<IAppInfo | undefined> {
+    const itemDocument = doc(this.firestore, collectionName, documentId);
+
+    return getDoc(itemDocument)
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          return docSnap.data() as IAppInfo; // Retorna os dados do documento
+        } else {
+          console.log('Documento não encontrado');
+          return undefined; // Retorna undefined se o documento não existir
+        }
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar documento:', error);
+        throw error; // Lança o erro para que possa ser tratado pelo chamador
+      });
+  }
+}
