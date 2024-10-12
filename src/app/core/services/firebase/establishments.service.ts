face488d2a7d296dfd8d4bd428e9e9a245c6e710
baptistelaldev <vbf.digi@gmail.com>
2024-10-11 21:02:02 -0300

    melhorias, no filtro e visualização da lista

--- src/app/core/services/firebase/establishments.service.ts
+++ src/app/core/services/firebase/establishments.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { getDocs, query, where } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 import { IShortEstablishment } from 'src/app/shared/models/Establishment';
 
@@ -14,6 +15,21 @@ export class EstablishmentsService {
 
   public getCollection(collectionName: string): Observable<IShortEstablishment[]> {
     const itemCollection = collection(this.firestore, collectionName);
-    return collectionData<any>(itemCollection)
+    return collectionData<any>(itemCollection);
+  }
+
+  async getCollectionFilteredBy(collectionName: string, field: string): Promise<IShortEstablishment[]> {
+    const itemCollection = collection(this.firestore, collectionName);
+    const q = query(itemCollection, where(field, '==', true));
+
+    const querySnapshot = await getDocs(q);
+
+    // Aqui, você pode garantir que os dados estão no formato de IShortEstablishment
+    const items: IShortEstablishment[] = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data() as IShortEstablishment // Asegure-se de que os dados correspondam à interface
+    }));
+
+    return items;
   }
 }
