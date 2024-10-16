153a6812b4d313acb6eabc24feeca1b1524bd4b8
baptistelaldev <vbf.digi@gmail.com>
2024-10-16 17:04:57 -0300

    idéias novas para a home

--- src/app/pages/tabs/inicio/inicio.page.ts
+++ src/app/pages/tabs/inicio/inicio.page.ts
@@ -1,24 +1,22 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { AlertController, AlertInput, IonContent, IonSelect, NavController, PopoverController } from '@ionic/angular';
+import { AlertController, IonContent, IonSelect, NavController, PopoverController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { map, Observable, Subscription, take } from 'rxjs';
 import { IShortEstablishment } from 'src/app/shared/models/Establishment';
 import { ILang } from 'src/app/shared/models/Lang';
-import { ISocialNetwork } from 'src/app/shared/models/Network';
 import * as AppStore from './../../../shared/store/app.state';
 import { Store } from '@ngrx/store';
-import { ITranslation } from 'src/app/shared/models/Translation';
 import { EstablishmentsService } from 'src/app/core/services/firebase/establishments.service';
 import { CollectionsEnum } from 'src/app/shared/enums/Collection';
 import * as moment from 'moment';
 import { UtilsService } from 'src/app/core/services/utils.service';
-import SwiperComponent, { Swiper } from 'swiper';
-import { ActivatedRoute } from '@angular/router';
+import { Swiper } from 'swiper';
 import { AnalyticsService } from 'src/app/core/services/firebase/analytics.service';
 import { AnalyticsEventnameEnum } from 'src/app/shared/enums/Analytics';
 import { IShortParking } from 'src/app/shared/models/IParking';
 import { ParkingsService } from 'src/app/core/services/firebase/parkings.service';
+import { EstablishmentTypeEnum } from 'src/app/shared/enums/EstablishmentType';
 
 @Component({
   selector: 'rgs-inicio',
@@ -26,6 +24,139 @@ import { ParkingsService } from 'src/app/core/services/firebase/parkings.service
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit, OnDestroy, AfterViewInit {
+
+  public selectedInfo: any;
+
+  public lenghts_to_save_time: {
+    list: IShortEstablishment[],
+    establishment_type: string,
+    text: any,
+    length: number,
+    plural: any
+  }[] = [
+    {
+      list: [],
+      establishment_type: 'ADEGA',
+      length: 0,
+      text: {
+        pt: 'Adega',
+        en: 'Wine Cellar',
+        es: 'Bodega'
+      },
+      plural: {
+        pt: 'Adegas',
+        en: 'Wine Cellars',
+        es: 'Bodegas'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'DOCERIA',
+      length: 0,
+      text: {
+        pt: 'Doceria',
+        en: 'Confectionery',
+        es: 'Dulcería'
+      },
+      plural: {
+        pt: 'Docerias',
+        en: 'Confectioneries',
+        es: 'Dulcerías'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'EMPORIO',
+      length: 0,
+      text: {
+        pt: 'Empório',
+        en: 'Emporium',
+        es: 'Empore'
+      },
+      plural: {
+        pt: 'Empórios',
+        en: 'Emporiums',
+        es: 'Emporios'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'HAMBURGUERIA',
+      length: 0,
+      text: {
+        pt: 'Hamburgueria',
+        en: 'Burger Joint',
+        es: 'Hamburguesería'
+      },
+      plural: {
+        pt: 'Hamburguerias',
+        en: 'Burger Joints',
+        es: 'Hamburgueserías'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'BAR',
+      length: 0,
+      text: {
+        pt: 'Bar',
+        en: 'Bar',
+        es: 'Bar'
+      },
+      plural: {
+        pt: 'Bares',
+        en: 'Bars',
+        es: 'Bares'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'RESTAURANTE',
+      length: 0,
+      text: {
+        pt: 'Restaurante',
+        en: 'Restaurant',
+        es: 'Restaurante'
+      },
+      plural: {
+        pt: 'Restaurantes',
+        en: 'Restaurants',
+        es: 'Restaurantes'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'PIZZARIA',
+      length: 0,
+      text: {
+        pt: 'Pizzaria',
+        en: 'Pizzeria',
+        es: 'Pizzería'
+      },
+      plural: {
+        pt: 'Pizzarias',
+        en: 'Pizzerias',
+        es: 'Pizzerías'
+      }
+    },
+    {
+      list: [],
+      establishment_type: 'CAFETERIA',
+      length: 0,
+      text: {
+        pt: 'Cafeteria',
+        en: 'Cafeteria',
+        es: 'Cafetería'
+      },
+      plural: {
+        pt: 'Cafeterias',
+        en: 'Cafeterias',
+        es: 'Cafeterías'
+      }
+    }
+  ]
+
+  public today: string = moment().format('LL');
 
   @ViewChild('establishmentsSwiper')
   swiperRef: ElementRef | undefined;
@@ -148,7 +279,7 @@ export class InicioPage implements OnInit, OnDestroy, AfterViewInit {
     }
   ]
 
-  public showParkingModal: boolean = false;
+  public showSpecificList: boolean = false;
 
   public currentLanguage: ILang;
   public currentLanguage$: Observable<ILang>;
@@ -262,6 +393,17 @@ export class InicioPage implements OnInit, OnDestroy, AfterViewInit {
     )
     .subscribe((establishments: IShortEstablishment[]) => {
       this.short_establishments = establishments;
+      this.lenghts_to_save_time = [...this.lenghts_to_save_time].map((option: any) => {
+        let list = this.short_establishments.filter((establishment: IShortEstablishment) => {
+          return establishment.mainType.value === option['establishment_type'];
+        })
+
+        option['list'] = list;
+        option['length'] = list.length;
+
+        return option;
+      })
+
     })
   }
 
@@ -364,6 +506,19 @@ export class InicioPage implements OnInit, OnDestroy, AfterViewInit {
     }
   }
 
+  public seeEstablishmentFromModal(establishment: IShortEstablishment, e: any): void {
+    if (establishment.isBuilding) {
+      e.preventDefault();
+    } else {
+      this.showSpecificList = false;
+
+      setTimeout(() => {
+        this.navCtrl.navigateForward(['/estabelecimento/' + establishment.value]);
+        this.store.dispatch(AppStore.setCurrentEstablishment({ establishment: establishment } ))
+      }, 100);
+    }
+  }
+
   public slideSwiperToStart(): void {
     this.swiper?.slideTo(0, 800);
   }
@@ -425,16 +580,18 @@ export class InicioPage implements OnInit, OnDestroy, AfterViewInit {
 
   }
 
-  public seeParking(show: boolean): void {
-    this.showParkingModal = show;
+  public seeSpecficList(show: boolean, info: any): void {
+    this.showSpecificList = show;
+    this.selectedInfo = info;
   }
 
   public modalHasDismissed(e: any) {
-    this.showParkingModal = false;
+    this.showSpecificList = false;
+    this.selectedInfo = null;
   }
 
   public closeModal(): void {
-    this.showParkingModal = false;
+    this.showSpecificList = false;
   }
 
   public callParking(parking: any, e: any) {
