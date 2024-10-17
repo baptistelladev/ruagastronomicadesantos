dc5168fefa223cfe19cad0a319fa8d427b14f109
baptistelaldev <vbf.digi@gmail.com>
2024-10-16 22:01:54 -0300

    removendo mp4 no mobile, ajuste de classes

---
+++ src/app/shared/components/adsense/adsense.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rgs-adsense',
+  templateUrl: './adsense.component.html',
+  styleUrls: ['./adsense.component.scss'],
+})
+export class AdsenseComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
